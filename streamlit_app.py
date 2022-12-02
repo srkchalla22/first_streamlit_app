24398384c0f5d566d08721e7535b82ffa27d000c
Sreenu Challa <119619490+srkchalla22@users.noreply.github.com>
2022-12-02 11:57:46 -0600

    Update streamlit_app.py
    Choice option added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,18 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
-fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-#streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+#import requests
+#fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+##streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+##take the json version of the response and normalize it
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+##output it the screen as a table
+#streamlit.dataframe(fruityvice_normalized)
+
+#Below code is choice based
+fruit_choice= streamlit.text_input('What fruit would you like the information about?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
+
+import requests
+fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
