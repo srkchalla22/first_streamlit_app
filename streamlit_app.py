1648a5e05812509f5c1d4b68b39ed1ad0e2777f2
Sreenu Challa <119619490+srkchalla22@users.noreply.github.com>
2022-12-03 11:13:57 -0600

    Update streamlit_app.py
    Added function for snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,20 +71,30 @@ streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
 #Fecth all instead of one
-my_data_rows = my_cur.fetchall()
+#my_data_rows = my_cur.fetchall()
 #streamlit.text("The fruit load list containse:")
 #streamlit.text(my_data_row)
 
 #To display in tabluar format
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #Below code is choice based
 fruit_choice= streamlit.text_input('What fruit would you like add?','Kiwi')
