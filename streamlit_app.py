65165685193cf21a264da554480e1c31e28977cb
Sreenu Challa <119619490+srkchalla22@users.noreply.github.com>
2022-12-03 10:37:41 -0600

    Update streamlit_app.py
    Fixed indetation

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ streamlit.dataframe(fruits_to_show)
 #create a repeatable code block(called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-	fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
-	return fruitvice_normalized
+    fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
+    return fruitvice_normalized
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
